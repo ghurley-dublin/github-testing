a3f8fb3b761be285b6b29ee07670d59a9e617122
ghurley <ghurley@murex.com>
2016-06-01 14:24:43 +0100

    Check for changes to key files comparing the 2nd revision to the HEAD - but make the job fail if changes found

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
-git diff --name-only `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE README.md
+git diff --exit-code --name-only `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE README.md
