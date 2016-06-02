46330d0eb5b516d7c17293b578a854491bf3946e
ghurley <ghurley@murex.com>
2016-06-02 19:14:06 +0100

    Compare this commit with the last commit

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
-echo "Email to $EMAIL ..."
 echo "Checking for modifications to files that should not be changed"
-git diff --exit-code --name-only `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE
+git diff --exit-code HEAD^1 HEAD LICENSE
