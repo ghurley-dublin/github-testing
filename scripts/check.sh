482582f785c1c9fc4e302f3f7aa5c516a11d7446
ghurley <ghurley@murex.com>
2016-06-02 12:51:00 +0100

    Configure email to send to

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
+echo "Email to $EMAIL ..."
 echo "Checking for modifications to files that should not be changed"
 git diff --exit-code --name-only `git rev-list HEAD | tail -n 2 | head -n 1` HEAD LICENSE
