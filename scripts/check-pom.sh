1abc80d8cd1e0757af9f57d9eb69ede1ec6aa005
ghurley <ghurley@murex.com>
2016-06-03 17:13:46 +0100

    Simple POM dependency check

--- scripts/check-pom.sh
+++ scripts/check-pom.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
+set -x
 echo "Checking POM dependencies for changes..."
 
 mkdir temp
@@ -7,5 +8,11 @@ mvn dependency:tree -Doutput=temp/current-tree.txt
 
 checkout HEAD^1
 mvn dependency:tree -Doutput=temp/old-tree.txt
+
+echo "Current..."
+cat temp/current-tree.txt
+echo "------------------------------------"
+echo "Old..."
+cat temp/old-tree.txt
 
 diff --exit-code temp/current-tree.txt temp/old-tree.txt
