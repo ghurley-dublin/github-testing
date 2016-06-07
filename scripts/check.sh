613a03a2cf21f47d591569d7a3ca985ebaacf97d
ghurley <ghurley@murex.com>
2016-06-07 13:03:46 +0100

    scripts folder should not be modified. Improve message in log

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,3 +2,10 @@
 # Determine if the following files changed in the branch
 echo "Checking for modifications to files that should not be changed..."
 git diff --exit-code HEAD^1 HEAD LICENSE scripts
+if [ $? -eq 0 ]; then
+	echo "SUCCESS: Key files unchanged in last commit"
+else
+	echo "FAILURE: Key file(s) modified in last commit" >&2
+fi
+exit $?
+
