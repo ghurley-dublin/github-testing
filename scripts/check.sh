8cbeed031233f27de737950c14b1ce4b97cd9051
ghurley <ghurley@murex.com>
2016-06-07 13:07:36 +0100

    scripts folder should not be modified

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,8 +4,10 @@ echo "Checking for modifications to files that should not be changed..."
 git diff --exit-code HEAD^1 HEAD LICENSE scripts
 if [ $? -eq 0 ]; then
 	echo "SUCCESS: Key files unchanged in last commit"
+	exit 0
 else
 	echo "FAILURE: Key file(s) modified in last commit" >&2
+	exit 1
 fi
-exit $?
+
 
