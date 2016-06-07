0d53be89a50e43c348ac17ace54ebdc69c4cee5b
ghurley <ghurley@murex.com>
2016-06-07 12:51:30 +0100

    scripts folder should not be modified

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
-echo "Checking for modifications to files that should not be changed"
-git diff --exit-code HEAD^1 HEAD LICENSE
+echo "Checking for modifications to files that should not be changed..."
+git diff --exit-code HEAD^1 HEAD LICENSE scripts
