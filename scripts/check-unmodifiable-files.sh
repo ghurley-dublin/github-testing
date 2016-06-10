eaa8b7ae34dd1cd8dbaaf909206a790a70c07554
ghurley <ghurley@murex.com>
2016-06-10 10:27:22 +0100

    Use Travis PULL_REQUEST variable

--- scripts/check-unmodifiable-files.sh
+++ scripts/check-unmodifiable-files.sh
@@ -3,7 +3,15 @@
 echo "-----------------------------------------------------------"
 echo "Checking for modifications to files that should not be changed..."
 echo "-----------------------------------------------------------"
-git diff --exit-code HEAD^1 HEAD LICENSE
+echo "Value of env variable TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
+if [ "${TRAVIS_PULL_REQUEST}" = true]; then
+	echo "Comparing HEAD against pending commit..."
+	git diff --exit-code HEAD LICENSE
+else
+	echo "Comparing HEAD against previous commit ..."
+	git diff --exit-code HEAD^1 HEAD LICENSE
+fi
+
 if [ $? -eq 0 ]; then
 	echo "SUCCESS: Files that shouldn't be modified were not changed in last commit"
 	exit 0
