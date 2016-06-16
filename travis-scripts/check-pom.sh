4c4c4f9b83886540f802bd7aae332d523d33516d
ghurley-dublin <gar_hurley@yahoo.com>
2016-06-16 10:27:56 +0100

    Test script changes (#5)
    * Disable Travis checks
    
    * POM check for master only; Disable LICENSE check
    
    * POM check for master only; Disable LICENSE check
    
    * Fix silly YAML issue
    
    * POM check for master only; Disable LICENSE check

---
+++ travis-scripts/check-pom.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Determine if the following files changed in the branch
+if [ "${TRAVIS_PULL_REQUEST}" = false ]; then
+	echo "-----------------------------------------------------------"
+	echo "Checking POM dependencies for changes..."
+	echo "-----------------------------------------------------------"
+	mkdir temp
+	mvn dependency:tree -Doutput=temp/current-tree.txt
+
+	git checkout HEAD^1
+	mvn dependency:tree -Doutput=temp/old-tree.txt
+
+	echo "---- Latest dependencies..."
+	cat temp/current-tree.txt
+	echo "------------------------------------"
+	echo "---- Previous dependencies..."
+	cat temp/old-tree.txt
+	echo "------------------------------------"
+
+	DIFF=`diff -q temp/current-tree.txt temp/old-tree.txt`
+
+	if [ -n "$DIFF" ]; then
+		echo "POM dependency tree changed in latest commit"
+		echo "Side-by-side diff (left=LATEST      right=PREVIOUS)"
+		diff -y temp/current-tree.txt temp/old-tree.txt
+		echo "STATUS -- WARNING: POM changes detected"
+	else
+		echo "STATUS -- SUCCESS: POM dependencies unchanged"
+	fi
+
+	git checkout master
+	echo "-----------------------------------------------------------"
+fi
--- travis-scripts/check-unmodifiable-files.sh
+++ travis-scripts/check-unmodifiable-files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Determine if the following files changed in the branch
+UNMODIFIABLE="LICENSE travis-scripts"
 echo "-----------------------------------------------------------"
 echo "Checking for modifications to files that should not be changed..."
 echo "-----------------------------------------------------------"
@@ -12,10 +13,10 @@ else
 fi
 
 if [ $? -eq 0 ]; then
-	echo "SUCCESS: Files that shouldn't be modified were not changed in last commit"
+	echo "SUCCESS: Files '$UNMODIFIABLE' that shouldn't be modified unchanged in last commit"
 	exit 0
 else
-	echo "FAILURE: Files that shouldn't be modified were unexpectedly modified in last commit" >&2
+	echo "FAILURE: Files '$UNMODIFIABLE' that shouldn't be modified were unexpectedly modified in last commit" >&2
 	exit 1
 fi
 echo "-----------------------------------------------------------"
